c2a2f3029402848b07c3da84c2d960733a2408a8
cristian <thepany96@gmail.com>
2018-11-13 15:29:35 -0400

    fix errors

--- src/components/About.vue
+++ src/components/About.vue
@@ -14,7 +14,7 @@
                  <p class="mt-4">Comencé a desarrollar software cuando acababa de entrar a la universidad. Mis primeros programas se escribieron en C y Java. Asistí a la universidad Universidad Católica del Cibao (UCATECI). Entre 2014~2017 fui a diferentes charlas de tecnologia, seminarios y hackathones. participar en un programa de certificación impartido por altice dominicana para el desarrollo de aplicaciones básicas y avanzadas</p>
 
                  <h2 class="mt-12 text-medium-purple text-2xl font-bold">Desarrollo web</h2>
-                 <p class="mt-4">Mi enfoque principal como desarrollador de software es el desarrollo web, lo que he estado haciendo desde inicios del 2016. Mis primeros proyectos fueron construidos con HTML, CSS, JavaScript, PHP y MySQL. Curiosamente, mi conjunto de herramientas sigue siendo el mismo hoy. Estos días mi framework PHP de elección es <a class="text-blue-dark" href="https://laravel.com/">Laravel</a>, He estado experimentando con <a class="text-blue-dark" href="https://tailwindcss.com">TailwindCSS</a> un framework css de utilidad para el desarrollo rápido de la interfaz de usuario. Estos días estoy haciendo mucho de mi trabajo de JavaScript con <a class="text-blue-dark" href="https://vuejs.org/">Vue.js</a>.</p>
+                 <p class="mt-4">Mi enfoque principal como desarrollador de software es el desarrollo web, lo que he estado haciendo desde inicios del 2016. Mis primeros proyectos fueron construidos con HTML, CSS, JavaScript, PHP y MySQL. Curiosamente, mi conjunto de herramientas sigue siendo el mismo hoy.En estos días mi framework PHP de elección es <a class="text-blue-dark" href="https://laravel.com/">Laravel</a>, He estado experimentando con <a class="text-blue-dark" href="https://tailwindcss.com">TailwindCSS</a> un framework css de utilidad para el desarrollo rápido de la interfaz de usuario. Mucho de mi trabajo de JavaScript lo hago con <a class="text-blue-dark" href="https://vuejs.org/">Vue.js</a>.</p>
                  <p class="mt-4">Como desarrollador, valoro el código bien escrito, es fácil de leer y no es innecesariamente complejo. No veo las pruebas automatizadas como opcionales, sino más bien una parte clave para escribir un buen código. Aprecio los siguientes estándares de codificación de la industria. Me refiero a aprender y mejorar continuamente asistiendo a conferencias, leyendo libros y blogs, viendo screencast y siguiendo a expertos de la industria.</p>
              </div>
 
@@ -33,7 +33,3 @@
         }
     }
 </script>
-
-<style scoped>
-
-</style>
